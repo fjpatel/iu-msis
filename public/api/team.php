9d279e8d7f1de27767afbc51e414d5191e3c6a1b
Felicia <fjpatel@iu.edu>
2018-10-01 18:44:37 -0400

    changed getAll to fetchAll

--- public/api/team.php
+++ public/api/team.php
@@ -9,7 +9,7 @@ require '../../app/common.php';
 //}
 
 // 1. Go to the database and get all work associated with the $taskId
-$teams = Team::getAll();
+$teams = Team::fetchAll();
 
 // 2. Convert to JSON
 $json = json_encode($teams);
