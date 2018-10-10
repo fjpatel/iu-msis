c68b18c7d7a50a2e78e78d5c331865b615095d95
Felicia <fjpatel@iu.edu>
2018-10-10 12:20:17 -0400

    created WorkHours.php and updated other api endpoints

--- public/api/team.php
+++ public/api/team.php
@@ -2,12 +2,6 @@
 
 require '../../app/common.php';
 
-//$taskId = intval($_GET['taskId'] ?? 0);
-
-//if ($taskId < 1) {
-  //throw new Exception('Invalid Task ID');
-//}
-
 // 1. Go to the database and get all work associated with the $taskId
 $teams = Team::fetchAll();
 
